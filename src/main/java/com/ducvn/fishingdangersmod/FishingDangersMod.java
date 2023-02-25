3681f1abae7613539ef8c066b98e36d8115f0e84
DUCVN <nguyenvietduc24@yahoo.com.vn>
2023-02-25 13:23:30 +0700

    1.19.2

--- src/main/java/com/ducvn/fishingdangersmod/FishingDangersMod.java
+++ src/main/java/com/ducvn/fishingdangersmod/FishingDangersMod.java
@@ -1,20 +1,16 @@
 package com.ducvn.fishingdangersmod;
 
 import com.ducvn.fishingdangersmod.config.FishingDangersConfig;
-import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -30,45 +26,33 @@ public class FishingDangersMod
     public static final String MODID = "fishingdangers";
 
     public FishingDangersMod() {
-        // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-        // Register the enqueueIMC method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
-        // Register the processIMC method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
-        // Register the doClientStuff method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        // Register the commonSetup method for modloading
+        modEventBus.addListener(this::commonSetup);
 
         // Register ourselves for server and other game events we are interested in
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, FishingDangersConfig.SPEC, "FishingDangers-common.toml");
         MinecraftForge.EVENT_BUS.register(this);
     }
 
-    private void setup(final FMLCommonSetupEvent event)
+    private void commonSetup(final FMLCommonSetupEvent event)
     {
     }
 
-    private void doClientStuff(final FMLClientSetupEvent event) {
-    }
-
-    private void enqueueIMC(final InterModEnqueueEvent event)
-    {
-    }
-
-    private void processIMC(final InterModProcessEvent event)
-    {
-    }
     // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
-    public void onServerStarting(FMLServerStartingEvent event) {
+    public void onServerStarting(ServerStartingEvent event)
+    {
     }
 
-    // You can use EventBusSubscriber to automatically subscribe events on the contained class (this is subscribing to the MOD
-    // Event bus for receiving Registry Events)
-    @Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
-    public static class RegistryEvents {
+    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents
+    {
         @SubscribeEvent
-        public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
+        public static void onClientSetup(FMLClientSetupEvent event)
+        {
         }
     }
 }
--- src/main/java/com/ducvn/fishingdangersmod/events/FishingDangersEvents.java
+++ src/main/java/com/ducvn/fishingdangersmod/events/FishingDangersEvents.java
@@ -2,21 +2,20 @@ package com.ducvn.fishingdangersmod.events;
 
 import com.ducvn.fishingdangersmod.FishingDangersMod;
 import com.ducvn.fishingdangersmod.config.FishingDangersConfig;
-import net.minecraft.block.Blocks;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityType;
-import net.minecraft.entity.effect.LightningBoltEntity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.entity.projectile.FishingBobberEntity;
-import net.minecraft.item.FishingRodItem;
-import net.minecraft.util.Hand;
-import net.minecraft.util.Util;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.text.Color;
-import net.minecraft.util.text.StringTextComponent;
-import net.minecraft.util.text.Style;
-import net.minecraft.world.World;
-import net.minecraft.world.server.ServerWorld;
+import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.Style;
+import net.minecraft.network.chat.TextColor;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LightningBolt;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.projectile.FishingHook;
+import net.minecraft.world.item.FishingRodItem;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -30,15 +29,15 @@ public class FishingDangersEvents {
 
     @SubscribeEvent
     public static void FishingDangers(PlayerInteractEvent.RightClickItem event){
-        World world = event.getWorld();
-        PlayerEntity playerEntity = event.getPlayer();
+        Level world = event.getLevel();
+        Player playerEntity = event.getEntity();
         if (!world.isClientSide){
-            if (playerEntity.getItemInHand(Hand.MAIN_HAND).getItem() instanceof FishingRodItem
-            || playerEntity.getItemInHand(Hand.OFF_HAND).getItem() instanceof FishingRodItem){
+            if (playerEntity.getItemInHand(InteractionHand.MAIN_HAND).getItem() instanceof FishingRodItem
+            || playerEntity.getItemInHand(InteractionHand.OFF_HAND).getItem() instanceof FishingRodItem){
                 if (playerEntity.fishing != null && playerEntity.fishing.biting){
                     Random random = new Random();
                     if (random.nextDouble() < FishingDangersConfig.dangers_appear_chance.get()){
-                        FishingBobberEntity fishingBobberEntity = playerEntity.fishing;
+                        FishingHook fishingBobberEntity = playerEntity.fishing;
                         Entity entity = null;
                         List<? extends String> dangersList = FishingDangersConfig.dangers_list.get();
                         List<? extends Double> dangersChance = FishingDangersConfig.chance_list.get();
@@ -70,35 +69,36 @@ public class FishingDangersEvents {
                         else {
                             if (dangerName != null){
                                 if (dangerName.equals("minecraft:anvil")){
-                                    playerEntity.sendMessage(new StringTextComponent("You just pulled something heavy out of water, better watch your head"), Util.NIL_UUID);
+                                    playerEntity.sendSystemMessage(Component.literal("You just pulled something heavy out of water, better watch your head"));
                                     world.setBlock(new BlockPos(playerEntity.getX(), playerEntity.getY() + 5D, playerEntity.getZ()),
                                             Blocks.DAMAGED_ANVIL.defaultBlockState(),1);
                                 }
                                 else {
                                     if (dangerName.equals("minecraft:player")){
                                         Random roll = new Random();
-                                        List<PlayerEntity> playerList = new ArrayList<>(world.players());
-                                        playerList.remove(event.getPlayer());
+                                        List<Player> playerList = new ArrayList<>(world.players());
+                                        playerList.remove(event.getEntity());
                                         if (playerList.size() > 0) {
-                                            PlayerEntity unluckyPlayer = playerList.get(roll.nextInt(playerList.size()));
-                                            while (unluckyPlayer.level != event.getPlayer().level) {
+                                            Player unluckyPlayer = playerList.get(roll.nextInt(playerList.size()));
+                                            while (unluckyPlayer.level != event.getEntity().level) {
                                                 unluckyPlayer = playerList.get(roll.nextInt(playerList.size()));
                                             }
                                             if (unluckyPlayer.isAlive() && !unluckyPlayer.isCreative() && !unluckyPlayer.isSpectator()){
                                                 unluckyPlayer.teleportTo(fishingBobberEntity.getX(), fishingBobberEntity.getY(), fishingBobberEntity.getZ());
-                                                StringTextComponent name = new StringTextComponent(unluckyPlayer.getName().getString());
-                                                name.setStyle(Style.EMPTY.withColor(Color.fromRgb(5636095)));
-                                                playerEntity.sendMessage((new StringTextComponent("Look like ")
+                                                Component name = Component.literal(unluckyPlayer.getName().getString()).withStyle(
+                                                        Style.EMPTY.withColor(TextColor.fromRgb(5636095))
+                                                );
+                                                playerEntity.sendSystemMessage(Component.literal("Look like ")
                                                                 .append(name)
-                                                                .append(" like your bait")), Util.NIL_UUID);
-                                                ((ServerWorld) world).updateChunkPos(unluckyPlayer);
+                                                                .append(" like your bait"));
+                                                ((ServerLevel) world).addDuringTeleport(unluckyPlayer);
                                             }
                                         }
                                     }
                                     else {
                                         if (dangerName.equals("minecraft:lightning")){
-                                            playerEntity.sendMessage(new StringTextComponent("You just took the wrath of Poseidon"), Util.NIL_UUID);
-                                            LightningBoltEntity lightningBoltEntity = new LightningBoltEntity(EntityType.LIGHTNING_BOLT, world);
+                                            playerEntity.sendSystemMessage(Component.literal("You just took the wrath of Poseidon"));
+                                            LightningBolt lightningBoltEntity = new LightningBolt(EntityType.LIGHTNING_BOLT, world);
                                             lightningBoltEntity.setPos(playerEntity.getX(), playerEntity.getY(), playerEntity.getZ());
                                             world.addFreshEntity(lightningBoltEntity);
                                         }
